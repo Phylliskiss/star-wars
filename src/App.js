37ba6deba829b8d167453c8a4980cc4b25995db0
phylliskiss <kissehphyllis@gmail.com>
2023-03-20 14:18:01 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        "Codetrain is awesome, react works!!!!” 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+Star-wars        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
